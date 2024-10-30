79d07d9b148130916ab3ab4765d50975e9487551
kkirrin <khlebnikov-2016@mail.ru>
2024-10-30 12:49:29 +1000

    feat: закончил tabs

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ import { initHorizontalScroll } from './module/horizontalScroll.js';
 import { initCounterAnimation } from './module/counter-animation.js';
 import { initAllComfortSlider } from './module/sliders.js';
 import { initBtnOpenCardApart } from './module/btn-apart.js';
+import { initTabs } from './module/tabs.js';
 
 window.addEventListener('DOMContentLoaded', () => {
     console.log('подключен скрипт main.js');
@@ -16,6 +17,7 @@ window.addEventListener('DOMContentLoaded', () => {
     initCounterAnimation();
     initAllComfortSlider();
     initBtnOpenCardApart();
+    initTabs();
 
     const wow = new WOW({
         boxClass: 'wow',
